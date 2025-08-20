4fb24672a90970eb3bbdb25d0bf8d7e35506430e
Saidi Abasi <31949702+abasi@users.noreply.github.com>
2025-08-20 16:12:37 +0300

    first commit

---
+++ loops.py
@@ -0,0 +1,12 @@
+#using list 
+for x in [0,1,2,3,4,5,6]:
+    print(x)
+ 
+ #using range   
+for x in range(6):
+    print(x)
+
+#using rangevarible on list  
+names=["amina", "harry", "Hamisi", "kange", "ndobe"]
+for name in names:
+    print(name)
